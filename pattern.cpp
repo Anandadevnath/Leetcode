ad1346e771714ded51b3ea98ee0ffc5e289b33ab
Ananda Dev Nath <thunderhorizon18@gmail.com>
2025-01-14 00:45:44 +0600

    pattern5

--- pattern.cpp
+++ pattern.cpp
@@ -45,9 +45,21 @@ void pattern4(int n)
     cout << "\n";
   }
 }
+void pattern5(int n)
+{
+  for (int i = n; i > 0; i--)
+  {
+    for (int j = i; j > 0; j--)
+    {
+      cout << "* ";
+    }
+    cout << "\n";
+  }
+}
+
 int main()
 {
   int n;
   cin >> n;
-  pattern4(n);
+  pattern5(n);
 }

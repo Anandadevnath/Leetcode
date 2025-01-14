5288ef9de4c298ddfbfe21f0786cae6471aacdfa
Ananda Dev Nath <thunderhorizon18@gmail.com>
2025-01-14 14:11:08 +0600

    pattern10

--- pattern.cpp
+++ pattern.cpp
@@ -47,11 +47,93 @@ void pattern4(int n)
 }
 void pattern5(int n)
 {
-  for (int i = n; i > 0; i--)
+  // for (int i = n; i > 0; i--)
+  // {
+  //   for (int j = i; j > 0; j--)
+  //   {
+  //     cout << "* ";
+  //   }
+  //   cout << "\n";
+  // }
+
+  for (int i = 1; i <= n; i++)
   {
-    for (int j = i; j > 0; j--)
+    for (int j = 1; j <= n - i + 1; j++)
     {
       cout << "* ";
+    }
+    cout << "\n";
+  }
+}
+void pattern6(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n - i + 1; j++)
+    {
+      cout << j << " ";
+    }
+    cout << "\n";
+  }
+}
+void pattern7(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j <= n - i - 1; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 0; j < 2 * i + 1; j++)
+    {
+      cout << "*";
+    }
+    for (int j = 0; j <= n - i - 1; j++)
+    {
+      cout << " ";
+    }
+    cout << "\n";
+  }
+}
+void pattern8(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    // space
+    for (int j = 0; j <= i; j++)
+    {
+      cout << " ";
+    }
+    // stars
+    for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+    {
+      cout << "*";
+    }
+    // space
+    for (int j = 0; j <= i; j++)
+    {
+      cout << " ";
+    }
+    cout << "\n";
+  }
+}
+void pattern9(int n)
+{
+  pattern7(n);
+  pattern8(n);
+}
+void pattern10(int n)
+{
+  for (int i = 1; i <= 2 * n - 1; i++)
+  {
+    int stars = i;
+    if (i > n)
+    {
+      stars = 2 * n - i;
+    }
+    for (int j = 1; j <= stars; j++)
+    {
+      cout << "*";
     }
     cout << "\n";
   }
@@ -61,5 +143,5 @@ int main()
 {
   int n;
   cin >> n;
-  pattern5(n);
+  pattern10(n);
 }
